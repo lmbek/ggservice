e6f533e6aafd4530fab607fce5c282556d030701
Lars Morten Bek <lars@beksoft.dk>
2024-07-22 13:09:15 +0200

    Updated example with new version

--- example/go.mod
+++ example/go.mod
@@ -1,3 +1,5 @@
 module example
 
 go 1.22
+
+require github.com/lmbek/ggservice v0.0.1
--- example/main.go
+++ example/main.go
@@ -13,9 +13,23 @@ func main() {
 }
 
 func loadService() {
-	//alternative //s := new_ssg.NewService("SSG Service", 5*time.Second)
-	service := ggservice.New(&ggservice.Service{Name: "My Service", GracefulShutdownTime: 5 * time.Second})
-	err := service.Start(start, run, forcedTimeoutStop)
+	// creating new service with name and graceful shutdown time duration
+	service := ggservice.NewService("SSG Service", 5*time.Second)
+
+	// we can use Start, Stop and ForceShutdown on the service
+
+	// since Start is a blocking operation we would want to put Start, Stop or ForceShutdown into a goroutine
+	// if we want to stop the service ourselves, otherwise we would also just wait for interrupt
+	go func() {
+		time.Sleep(1 * time.Second)
+		// we can also use service.Stop or service.ForceShutdown, but notice service.Start is a blocking call
+		// we can put service.Start in a go routine and use waitgroups etc... your choice.
+		//service.Stop()          // waits till all operations are done (not waiting for graceful timer)
+		//service.ForceShutdown() // force shutdown immediately
+	}()
+
+	// starting the service (please note you can choose to not implement any of these by using nil instead)
+	err := service.Start(start, run, forceExit) // this is a blocking call
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,18 +44,12 @@ func start() error {
 // run loops from the application is started until it is stopped, terminated or ForceShutdown (please use with time.Sleep in between frames)
 func run() error {
 	fmt.Println("start of work")
-
-	// service will run the rest of the task if graceful shutdown timer is > 1
-	time.Sleep(7 * time.Second)
-	// service will force shutdown if the rest of the task length
-	// is > 5 (or whatever graceful shutdown timer is set to)
-	// time.Sleep(8 * time.Second)
-
+	time.Sleep(10 * time.Second) // note: if the graceful timer duration is below amount of work needed to be done, it will forceExit
 	fmt.Println("end of work")
 	return nil
 }
 
-// forcedStop is being run when the application is trying to force a shutdown (non-gracefully)
-func forcedTimeoutStop() {
-	log.Fatal(errors.New("forced stop: timeout"))
+// forceExit is being run when the application is trying to force a shutdown (non-gracefully)
+func forceExit() {
+	log.Fatalln(errors.New("forced stop: timeout"))
 }
